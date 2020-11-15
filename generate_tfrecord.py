4a17d2f0ce4f5ae921cb41465e368c7f839cc718
Malinee2029 <45258614+Malinee2029@users.noreply.github.com>
2020-11-15 19:30:07 +0700

    Update generate_tfrecord.py

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -29,19 +29,10 @@ FLAGS = flags.FLAGS
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'nine':
-        return 1
-    elif row_label == 'ten':
-        return 2
-    elif row_label == 'jack':
-        return 3
-    elif row_label == 'queen':
-        return 4
-    elif row_label == 'king':
-        return 5
-    elif row_label == 'ace':
-        return 6
-    else:
+        if row_label == 'Fire':
+            return 1
+        elif row_label == 'label':
+            return 2
         None
 
 
